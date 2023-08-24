b3d945a11eb6001b9b7b73cbc8ffd463e83bcbe2
mohammed shoaib <142919025+mohammedshoaib221@users.noreply.github.com>
2023-08-24 22:07:24 +0530

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,25 @@
+import random
+
+
+rubikscube=random.randint(0,5)
+
+print(rubikscube)
+
+
+if rubikscube==0:
+    print("the color is green")
+
+elif rubikscube==1:
+    print("the color is green")
+
+elif rubikscube==2:
+    print("The color is blue")
+     
+elif rubikscube==3:
+    print("The color is yellow")
+
+elif rubikscube==4:
+    print("The color is white")
+
+else:
+    print("the color is orange")
